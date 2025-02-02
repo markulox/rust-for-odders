941d4c2b0c1a27d2ee5a12de9726a11f73e3e099
Nopphon <nopphonyel@gmail.com>
2025-02-02 13:30:07 +0700

    style: update exercises ids;

--- exercises/ex04a/main.rs
+++ exercises/ex04a/main.rs
@@ -1,23 +1,4 @@
-// Implement count_tacos function here
-
 // End of implementation zone
-
-fn execute_me1() -> String {
-    let taco_box: [Option<&str>; 10] = [
-        Some("Beef taco"),
-        Some("Chicken taco"),
-        Some("Veggie taco"),
-        None,
-        Some("Beef taco"),
-        None,
-        None,
-        Some("Fruit taco"),
-        Some("Veggie taco"),
-        None,
-    ];
-
-    todo!();
-}
 
 // You can experiment your code in here!
 fn main(){
